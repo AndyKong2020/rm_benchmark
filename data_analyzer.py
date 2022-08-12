516093d1b6ba4079ae7ece6c0d77dc4976dcb39b
AndyKong2020 <andy.kong@nuaa.edu.cn>
2022-08-13 00:52:30 +0800

    Revert "[fix]"
    This reverts commit 8628a5dc303522479e6a79dfcd3a2e515f3ae31c.
    
    # Conflicts:
    #	data_analyzer.py

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,5 +1,8 @@
 # 作者:明月清风我
 # 时间:2022/7/15 21:17
+import pandas as pd
+from pandas import Series, DataFrame
+from pathlib import Path
 import numpy as np
 from shapely.geometry import Polygon
 
@@ -23,15 +26,10 @@ class ResultsAnalyzer:
     result_not_found = 0
 
     def __init__(self, dataset_path, results_path):
-        # self.dataset_path = dataset_path
-        # self.results_path = results_path
-        # self.dataset_object = source_data.SourceDataLabel(self.dataset_path)
-        # self.results_object = source_data.SourceDataLabel(self.results_path)
-        # self.dataset = self.dataset_object.label_dict
-        # self.results = self.results_object.label_dict
-        self.dataset = dataset_path
-        self.results = results_path
-        print(self.dataset, self.results)
+        self.dataset_path = dataset_path
+        self.results_path = results_path
+        self.dataset = source_data.SourceDataLabel(dataset_path).label_dict
+        self.results = source_data.SourceDataLabel(results_path).label_dict
 
     def result_error(self):
         self.result_not_found = 0
@@ -51,46 +49,41 @@ class ResultsAnalyzer:
             return ((sum(data1[0][:])/4 - sum(data2[0][:])/4)**2 + (sum(data1[1][:])/4 - sum(data2[1][:])/4)**2)**0.5
 
         for dataset_path_key in self.dataset.keys():
-            dataset_path = self.dataset[dataset_path_key]
-            results_path = self.results[dataset_path_key]
-            print(dataset_path, results_path)
+            dataset_path = self.dataset.get(dataset_path_key)
+            results_path = self.results.get(dataset_path_key)
             if results_path is None:
                 self.result_not_found += 1
-                print(self.result_not_found, 'rnf')
                 continue
             with open(dataset_path, "r") as file:
                 dataset_coords = file.readlines()
+                dataset_coord = []
+                dataset_shpe = []
                 index: int
                 for index in range(len(dataset_coords)):
-                    dataset_coords[index] = [list(map(float, dataset_coords[index].split()[1:3])),
-                                             list(map(float, dataset_coords[index].split()[3:5])),
-                                             list(map(float, dataset_coords[index].split()[5:7])),
-                                             list(map(float, dataset_coords[index].split()[7:9]))]
+                    dataset_coords[index] = [dataset_coords[index].split()[1:3],
+                                             dataset_coords[index].split()[3:5],
+                                             dataset_coords[index].split()[5:7],
+                                             dataset_coords[index].split()[7:9]]
             with open(results_path, "r") as file:
                 results_coords = file.readlines()
+                results_coord = []
                 index: int
                 for index in range(len(results_coords)):
-                    results_coords[index] = [list(map(float, results_coords[index].split()[1:3])),
-                                             list(map(float, results_coords[index].split()[3:5])),
-                                             list(map(float, results_coords[index].split()[5:7])),
-                                             list(map(float, results_coords[index].split()[7:9]))]
+                    results_coords[index] = [results_coords[index].split()[1:3],
+                                             results_coords[index].split()[3:5],
+                                             results_coords[index].split()[5:7],
+                                             results_coords[index].split()[7:9]]
             detect_num_error[dataset_path_key] = len(results_coords) - len(dataset_coords)
             compare_array = np.zeros([len(results_coords), len(dataset_coords)])
-            print(compare_array, len(results_coords), len(dataset_coords))
             for row in range(len(results_coords)):
                 for column in range(len(dataset_coords)):
                     compare_array[row, column] = distance(results_coords[row], dataset_coords[column])
             match = []
-            flag = 0
-            print(compare_array, 'a')
             while compare_array.min() != 2:
                 min_index_tuple = divmod(np.argmax(compare_array), compare_array.shape[1])
                 match.append(min_index_tuple)
                 compare_array[min_index_tuple[0], :] = 2
                 compare_array[:, min_index_tuple[1]] = 2
-                flag += 1
-                print(flag)
-                print(compare_array)
             detect_coord_error[dataset_path_key] = []
             for index_tuple in match:
                 cover_rate = cal_area_2poly(results_coords[index_tuple[0]], dataset_coords[index_tuple[1]])/\
